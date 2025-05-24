e5349cc5e86d1b75aea5b526b9f7cc76af45a384
Rishil Kathiriya <rishil.the.analyst@gmail.com>
2025-05-24 15:49:55 +0530

    Add files via upload

---
+++ Python Selenium Atomated Web Scrapping/free_proxy.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://free-proxy-list.net/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+proxies = []
+for row in soup.select("table tbody tr")[:10]:
+    tds = row.find_all("td")
+    ip = tds[0].text
+    port = tds[1].text
+    https = tds[6].text
+    if https == 'yes':
+        proxies.append(f'{ip}:{port}')
+
+print(proxies)
