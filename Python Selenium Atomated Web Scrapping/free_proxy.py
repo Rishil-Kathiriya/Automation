c1b97471d876e2c141fa02bdfecdc37b25b9bbe3
Rishil Kathiriya <rishil.the.analyst@gmail.com>
2025-06-25 17:23:03 +0530

    Update free_proxy.py

--- Python Selenium Atomated Web Scrapping/free_proxy.py
+++ Python Selenium Atomated Web Scrapping/free_proxy.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 url = 'https://free-proxy-list.net/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 proxies = []
-
+ 
+#iterate through all proxies
 for row in soup.select("table tbody tr")[:10]:
     tds = row.find_all("td")
     ip = tds[0].text
@@ -15,7 +17,7 @@ for row in soup.select("table tbody tr")[:10]:
     if https == 'yes':
         proxies.append(f'{ip}:{port}')
 
-
+#print free proxies
 print(proxies)
 
 
